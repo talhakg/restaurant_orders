8ccf22449398de158ed396f0ee84497b11bb2b86
Talha Ghori <talhaghori18@gmail.com>
2025-09-30 00:04:27 -0400

    Added primary keys and not nulls to the main tables. Recreated schemas with drop table and included indexes

--- schema.sql
+++ schema.sql
@@ -1,27 +1,34 @@
+-- drop old tables if they exist to recreate them clearly
+-- if we rerun the schema file it won't throw an error now
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS menu;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS order_items;
+
 -- create the nessessary tables
 CREATE TABLE customers (
-    customer_id INT,
-    name TEXT,
+    customer_id INT PRIMARY KEY,
+    name TEXT NOT NULL,
     phone TEXT
 );
 
 CREATE TABLE menu (
-    item_id INT,
-    name TEXT,
-    price REAL
+    item_id INT PRIMARY KEY,
+    name TEXT NOT NULL,
+    price REAL NOT NULL
 );
 
 CREATE TABLE orders (
-    order_id INT,
-    customer_id INT,
-    status TEXT, -- opened, paid, and cancelled
-    opened TEXT -- text timestamp
+    order_id INT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'OPEN', -- opened, paid, and cancelled
+    opened TEXT NOT NULL -- text timestamp
 );
 
 CREATE TABLE order_items (
-    order_item_id INT,
-    order_id INT,
-    item_id INT,
-    qty INT,
-    unit_price REAL
+    order_item_id INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    item_id INT NOT NULL,
+    qty INT NOT NULL,
+    unit_price REAL NOT NULL
 );
