e044e1af99390b6789653256c6a092fbe3f12cb9
Talha Ghori <talhaghori18@gmail.com>
2025-10-01 22:55:21 -0400

    Populate and query by inserting starting rows and run simple sanity checks

---
+++ sanity.sql
@@ -0,0 +1,4 @@
+SELECT * FROM customers;
+SELECT * FROM menu ORDER BY price DESC;
+SELECT * FROM orders;
+SELECT * FROM order_items;
---
+++ seed_data.sql
@@ -0,0 +1,11 @@
+-- insert customer info
+INSERT INTO customers (customer_id, name, phone) VALUES
+  (1, 'Aisha Khan', '555-1111'),
+  (2, 'Mark Chen', '555-2222'),
+  (3, 'Sara Ali', '555-3333');
+
+-- insert menu info
+INSERT INTO menu (item_id, name, price) VALUES
+  (1, 'Margherita Pizza', 9.99),
+  (2, 'Pepperoni Pizza', 11.99),
+  (3, 'Iced Tea', 2.99);
